003a6ea17d70f3bff33a4540dec578e53e296750
Jeff Mathieu <jeff.mathieu@student.kuleuven.be>
2025-05-27 11:17:44 +0200

    init code

---
+++ src/main/java/com/jeffmathieu/game/Game.java
@@ -0,0 +1,29 @@
+package com.jeffmathieu.game;
+
+public class Game {
+    private Board board;
+    private int score;
+
+    public Game() {
+        board = new Board();
+        score = 0;
+        board.spawnRandomTile();
+        board.spawnRandomTile();
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void move(Direction dir) {
+        int gained = board.move(dir);
+        if (gained > 0) {
+            score += gained;
+            board.spawnRandomTile();
+        }
+    }
+}
